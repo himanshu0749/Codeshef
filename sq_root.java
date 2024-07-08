edefc9c149e5172892bb1bd3b4f55dc8fa93b271
Himanshu Bhadoria <136106889+himanshu0749@users.noreply.github.com>
2024-07-08 09:18:03 +0530

    Create sq_root.java

---
+++ sq_root.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+            int N = scanner.nextInt();
+            int squareRoot = (int) Math.floor(Math.sqrt(N));
+            System.out.println(squareRoot);
+        }
+
+	}
+}
